d8c93733e52fbe08aa30e9c1a9e9e444c4236eea
francovaro <francovaro@gmail.com>
2026-01-08 11:14:44 +0100

    first commit with project

---
+++ src/inference/inferencing.cpp
@@ -0,0 +1,193 @@
+/* The Clear BSD License
+ *
+ * Copyright (c) 2025 EdgeImpulse Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ *   * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+ * THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#
+#include "inference/inferencing.h"
+#include "edge-impulse-sdk/classifier/ei_run_classifier.h"
+#include "edge-impulse-sdk/dsp/numpy.hpp"
+#include "sensors/ei_camera.h"
+
+#if (defined(EI_CLASSIFIER_SENSOR) && (EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA))
+#error "This inference project is only for camera sensor"
+#endif
+
+static uint8_t snapshot_buf[EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT * 3] __attribute__((aligned(32)));
+// __attribute__((aligned(32), section(".ext_ram.bss")));
+
+static bool ei_run_inference(void);
+static bool ei_start_impulse(void);
+static bool ei_stop_impulse(void);
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr);
+
+typedef enum {
+    INFERENCE_STATE_RUNNING,
+    INFERENCE_STATE_SAMPLING,
+    INFERENCE_STATE_DATA_READY,
+    INFERENCE_STATE_STOP
+} inference_state_t;
+
+static inference_state_t state = INFERENCE_STATE_SAMPLING;
+
+/**
+ * @brief Inference state machine
+ * @return true
+ */
+bool ei_inference_sm(void)
+{
+    size_t out_size;
+    ei_start_impulse();
+    
+    state = INFERENCE_STATE_SAMPLING;
+
+    while(INFERENCE_STATE_STOP != state) {
+        switch(state){
+            case INFERENCE_STATE_SAMPLING:
+                ei_printf("Taking photo...\n");
+                // capture image from camera
+                if (ei_camera_capture(snapshot_buf, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, &out_size) != 0) {
+                    ei_printf("ERR: Failed to capture image from camera\n");
+                    state = INFERENCE_STATE_STOP;
+                    break;
+                }
+                memset(snapshot_buf, 0, sizeof(snapshot_buf));     
+            case INFERENCE_STATE_DATA_READY:
+                ei_printf("Data ready\n");
+                // run inference, not much to do in this example
+                state = INFERENCE_STATE_RUNNING;
+                break;
+            case INFERENCE_STATE_RUNNING:
+                ei_printf("run inference\n");
+                if (ei_run_inference() == false) {
+                    ei_printf("ERR: Inference failed\n");
+                    state = INFERENCE_STATE_STOP;
+                    break;
+                }
+                state = INFERENCE_STATE_SAMPLING;   // and back sampling
+                break;
+            case INFERENCE_STATE_STOP:  // in this example we never reach this state
+                                        // but could be useful for your application
+                break;
+        }
+    }
+
+    ei_stop_impulse();
+
+    return state;
+}
+
+/**
+ * @brief Run inference process
+ * @return true if successful
+ */
+static bool ei_run_inference(void)
+{
+    bool ret = true;
+    ei_impulse_result_t result = { 0};
+
+    signal_t features_signal;
+    features_signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+    features_signal.get_data = &ei_camera_get_data;
+
+    // invoke the impulse
+    EI_IMPULSE_ERROR res = run_classifier(&features_signal, &result, false);
+
+    if (res != 0) {
+        ei_printf("ERR: Failed to run classifier\n");
+        ei_printf("ERR: %d\n", res);
+        ret = false;
+    }
+    else {
+        display_results(&ei_default_impulse, &result);
+    }
+
+    return ret;
+}
+
+/**
+ * @brief Start inference process
+ * @return true if successful
+ */
+static bool ei_start_impulse(void)
+{
+    ei_printf("Edge Impulse start inferencing on Zephyr\n");
+
+    ei_printf("Inferencing settings:\n");
+    ei_printf("\tClassifier interval: %.2f ms.\n", (float)EI_CLASSIFIER_INTERVAL_MS);
+    ei_printf("\tInput frame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+    ei_printf("\tNumber of output classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
+
+    // let's start, we will continuously run inference
+    run_classifier_init();
+
+    return true;
+}
+
+/**
+ * @brief Stop inference process
+ * @return true if successful
+ */
+static bool ei_stop_impulse(void)
+{
+    ei_printf("Stopping inferencing\n");
+    state = INFERENCE_STATE_STOP;
+
+    return true;
+}
+
+/**
+ *
+ * @param offset
+ * @param length
+ * @param out_ptr
+ * @return
+ */
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    ei_printf(" ---- ei_camera_get_data offset %d length %d\n", offset, length);
+    // we already have a RGB888 buffer, so recalculate offset into pixel index
+    size_t pixel_ix = offset * 3;
+    size_t pixels_left = length;
+    size_t out_ptr_ix = 0;
+
+    while (pixels_left != 0) {
+        out_ptr[out_ptr_ix] = (snapshot_buf[pixel_ix] << 16) + (snapshot_buf[pixel_ix + 1] << 8) + snapshot_buf[pixel_ix + 2];
+
+        // go to the next pixel
+        out_ptr_ix++;
+        pixel_ix+=3;
+        pixels_left--;
+    }
+
+    // and done!
+    return 0;
+}
---
+++ src/sensors/camera_drv.c
@@ -0,0 +1,387 @@
+/* The Clear BSD License
+ *
+ * Copyright (c) 2025 EdgeImpulse Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ *   * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+ * THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ei_camera.h"
+#include <zephyr/device.h>
+#include <zephyr/drivers/video.h>
+
+#if !DT_HAS_CHOSEN(zephyr_camera)
+#error No camera chosen in devicetree. Missing "--shield" or "--snippet video-sw-generator" flag?
+#endif
+
+static int app_query_video_info(const struct device *const video_dev,
+				struct video_caps *const caps,
+				struct video_format *const fmt);
+static int app_setup_video_selection(const struct device *const video_dev,
+				     const struct video_format *const fmt);
+static int app_setup_video_format(const struct device *const video_dev,
+				  struct video_format *const fmt);
+static int app_setup_video_frmival(const struct device *const video_dev,
+				   struct video_format *const fmt);
+static int app_setup_video_buffers(const struct device *const video_dev,
+				   struct video_caps *const caps,
+				   struct video_format *const fmt);
+static bool RBG565ToRGB888(uint8_t *src_buf, uint8_t *dst_buf, uint32_t src_len);
+
+const static struct device *const video_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_camera));
+static struct video_buffer *vbuf = 
+&(struct video_buffer){		
+    .type = VIDEO_BUF_TYPE_OUTPUT
+};
+static struct video_format fmt = {
+    .type = VIDEO_BUF_TYPE_OUTPUT,
+};
+static struct video_caps caps = {
+    .type = VIDEO_BUF_TYPE_OUTPUT,
+};
+
+static void local_print(const char *format, ...) {
+    static char print_buf[1024] = { 0 };
+
+    va_list args;
+    va_start(args, format);
+    int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+    va_end(args);
+
+    if(r > 0) {
+        printf("%s", print_buf);
+    }
+}
+
+uint8_t* camera_drv_capture(size_t* out_size)
+{
+    int ret;
+
+	vbuf->type = VIDEO_BUF_TYPE_OUTPUT;
+
+    ret = video_dequeue(video_dev, &vbuf, K_FOREVER);
+    if (ret < 0) {
+        local_print("Unable to dequeue video buf\n");
+        return -1;
+    }
+	
+    *out_size = vbuf->bytesused;
+
+    return vbuf->buffer;
+}
+
+bool camera_drv_start_capture(void)
+{
+	int ret;
+
+	vbuf->type = VIDEO_BUF_TYPE_OUTPUT;
+
+	ret = video_enqueue(video_dev, vbuf);
+	if (ret < 0) {
+		local_print("Unable to requeue video buf\n");
+		return -2;
+	}
+
+	return (ret == 0);
+}
+
+static int app_query_video_info(const struct device *const video_dev,
+				struct video_caps *const caps,
+				struct video_format *const fmt)
+{
+	int ret;
+
+	local_print("Video device: %s", video_dev->name);
+
+	if (!device_is_ready(video_dev)) {
+		local_print("%s: video device is not ready\n", video_dev->name);
+		return -ENOSYS;
+	}
+
+	/* Get capabilities */
+	ret = video_get_caps(video_dev, caps);
+	if (ret < 0) {
+		local_print("Unable to retrieve video capabilities\n");
+		return ret;
+	}
+
+	local_print("- Capabilities:\n");
+	for (int i = 0; caps->format_caps[i].pixelformat; i++) {
+		const struct video_format_cap *fcap = &caps->format_caps[i];
+
+		local_print("  %d width [%u; %u; %u] height [%u; %u; %u]\n",
+			VIDEO_FOURCC_TO_STR(fcap->pixelformat),
+			fcap->width_min, fcap->width_max, fcap->width_step,
+			fcap->height_min, fcap->height_max, fcap->height_step);
+	}
+
+	/* Get default/native format */
+	ret = video_get_format(video_dev, fmt);
+	if (ret < 0) {
+		local_print("Unable to retrieve video format\n");
+	}
+
+	/* Adjust video format according to the configuration */
+	fmt->width = CONFIG_VIDEO_FRAME_WIDTH;
+	fmt->height = CONFIG_VIDEO_FRAME_HEIGHT;	
+    
+#if defined CONFIG_VIDEO_PIXEL_FORMAT
+	if (strcmp(CONFIG_VIDEO_PIXEL_FORMAT, "") != 0) {
+		fmt->pixelformat = VIDEO_FOURCC_FROM_STR(CONFIG_VIDEO_PIXEL_FORMAT);
+	}
+#else
+	fmt->pixelformat = VIDEO_FOURCC_FROM_STR(CAMERA_DEFAULT_PIXEL_FORMAT);
+#endif
+	return 0;
+}
+
+static int app_setup_video_frmival(const struct device *const video_dev,
+				   struct video_format *const fmt)
+{
+	struct video_frmival frmival = {};
+	struct video_frmival_enum fie = {
+		.format = fmt,
+	};
+	int ret;
+
+	local_print("- Supported frame intervals for the default format:\n");
+
+	while (video_enum_frmival(video_dev, &fie) == 0) {
+		if (fie.type == VIDEO_FRMIVAL_TYPE_DISCRETE) {
+			local_print("   %u/%u", fie.discrete.numerator, fie.discrete.denominator);
+		} else {
+			local_print("   [min = %u/%u; max = %u/%u; step = %u/%u]\n",
+				fie.stepwise.min.numerator, fie.stepwise.min.denominator,
+				fie.stepwise.max.numerator, fie.stepwise.max.denominator,
+				fie.stepwise.step.numerator, fie.stepwise.step.denominator);
+		}
+		fie.index++;
+	}
+
+	ret = video_get_frmival(video_dev, &frmival);
+	if (ret == -ENOTSUP || ret == -ENOSYS) {
+		local_print("The video source does not support frame rate control\n");
+	} else if (ret < 0) {
+		local_print("Error while getting the frame interval\n");
+		return ret;
+	} else if (ret == 0) {
+		local_print("- Default frame rate : %f fps\n",
+			1.0 * frmival.denominator / frmival.numerator);
+	}
+
+	return 0;
+}
+
+static int app_setup_video_selection(const struct device *const video_dev,
+				     const struct video_format *const fmt)
+{
+	struct video_selection sel = {
+		.type = VIDEO_BUF_TYPE_OUTPUT,
+	};
+	int ret;
+
+	/* Set the crop setting only if configured */
+	if (CONFIG_VIDEO_SOURCE_CROP_WIDTH > 0) {
+		sel.target = VIDEO_SEL_TGT_CROP;
+		sel.rect.left = CONFIG_VIDEO_SOURCE_CROP_LEFT;
+		sel.rect.top = CONFIG_VIDEO_SOURCE_CROP_TOP;
+		sel.rect.width = CONFIG_VIDEO_SOURCE_CROP_WIDTH;
+		sel.rect.height = CONFIG_VIDEO_SOURCE_CROP_HEIGHT;
+
+		ret = video_set_selection(video_dev, &sel);
+		if (ret < 0) {
+			local_print("Unable to set selection crop\n");
+			return ret;
+		}
+
+		local_print("Crop window set to (%u,%u)/%ux%u",
+			sel.rect.left, sel.rect.top, sel.rect.width, sel.rect.height);
+	}
+
+
+	/*
+	 * Check (if possible) if targeted size is same as crop
+	 * and if compose is necessary
+	 */
+	sel.target = VIDEO_SEL_TGT_CROP;
+	ret = video_get_selection(video_dev, &sel);
+	if (ret < 0 && ret != -ENOSYS) {
+		local_print("Unable to get selection crop\n");
+		return ret;
+	}
+
+	if (ret == 0 && (sel.rect.width != fmt->width || sel.rect.height != fmt->height)) {
+		sel.target = VIDEO_SEL_TGT_COMPOSE;
+		sel.rect.left = 0;
+		sel.rect.top = 0;
+		sel.rect.width = fmt->width;
+		sel.rect.height = fmt->height;
+
+		ret = video_set_selection(video_dev, &sel);
+		if (ret < 0 && ret != -ENOSYS) {
+			local_print("Unable to set selection compose\n");
+			return ret;
+		}
+
+		local_print("Compose window set to (%u,%u)/%ux%u\n",
+			sel.rect.left, sel.rect.top, sel.rect.width, sel.rect.height);
+	}
+
+	return 0;
+}
+
+static int app_setup_video_format(const struct device *const video_dev,
+				  struct video_format *const fmt)
+{
+	int ret;
+
+	local_print("- Video format: %d %ux%u\n",
+		VIDEO_FOURCC_TO_STR(fmt->pixelformat), fmt->width, fmt->height);
+
+	ret = video_set_compose_format(video_dev, fmt);
+	if (ret < 0) {
+		local_print("Unable to set format\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int app_setup_video_buffers(const struct device *const video_dev,
+				   struct video_caps *const caps,
+				   struct video_format *const fmt)
+{
+	int ret;
+
+	/* Alloc video buffers and enqueue for capture */
+	if (caps->min_vbuf_count > CONFIG_VIDEO_BUFFER_POOL_NUM_MAX) {
+		local_print("Not enough buffers to start streaming\n");
+		return -EINVAL;
+	}
+
+	for (int i = 0; i < CONFIG_VIDEO_BUFFER_POOL_NUM_MAX; i++) {
+		struct video_buffer *vbuf;
+
+		/*
+		 * For some hardwares, such as the PxP used on i.MX RT1170 to do image rotation,
+		 * buffer alignment is needed in order to achieve the best performance
+		 */
+		local_print("Allocating video buffer %d size %u\n", i, fmt->size);
+		vbuf = video_buffer_aligned_alloc(fmt->size, CONFIG_VIDEO_BUFFER_POOL_ALIGN,
+						  K_NO_WAIT);
+		if (vbuf == NULL) {
+			local_print("Unable to alloc video buffer\n");
+			return -ENOMEM;
+		}
+
+		vbuf->type = VIDEO_BUF_TYPE_OUTPUT;
+
+		ret = video_enqueue(video_dev, vbuf);
+		if (ret < 0) {
+			local_print("Failed to enqueue video buffer\n");
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int camera_drv_init(uint16_t width, uint16_t height)
+{
+    int ret;
+
+    /* taken from app_query_video_info in samples/drivers/video/capture/src/main.c */
+	local_print("app_query_video_info\n");
+    ret = app_query_video_info(video_dev, &caps, &fmt);
+	if (ret < 0) {
+        local_print("Error %d\n", ret);
+		return ret;
+	}
+
+    /* taken from app_setup_video_selection in samples/drivers/video/capture/src/main.c */
+	local_print("app_setup_video_selection\n");
+	ret = app_setup_video_selection(video_dev, &fmt);
+	if (ret < 0) {
+        local_print("Error %d\n", ret);
+		return ret;
+	}
+    
+    /* taken from app_setup_video_format in samples/drivers/video/capture/src/main.c */
+	local_print("app_setup_video_format\n");
+	ret = app_setup_video_format(video_dev, &fmt);
+	if (ret < 0) {
+        local_print("Error %d\n", ret);
+		return ret;
+	}
+
+    /* taken from app_setup_video_frmival in samples/drivers/video/capture/src/main.c */
+	local_print("app_setup_video_frmival\n");
+    ret = app_setup_video_frmival(video_dev, &fmt);
+	if (ret < 0) {
+        local_print("Error %d\n", ret);
+		return ret;
+	}
+
+    /* taken from app_setup_video_buffers in samples/drivers/video/capture/src/main.c */
+	local_print("app_setup_video_buffers\n");
+    ret = app_setup_video_buffers(video_dev, &caps, &fmt);
+	if (ret < 0) {
+        local_print("Error %d\n", ret);
+		return ret;
+	}
+
+    /* taken from video_stream_start in samples/drivers/video/capture/src/main.c */
+	local_print("video_stream_start\n");
+	ret = video_stream_start(video_dev, VIDEO_BUF_TYPE_OUTPUT);
+	if (ret < 0) {
+		local_print("Unable to start capture (interface)\n");
+		return ret;
+	}
+
+	local_print("video_stream_start\n");
+
+    return ret;
+}
+
+static bool RBG565ToRGB888(uint8_t *src_buf, uint8_t *dst_buf, uint32_t src_len)
+{
+    uint8_t hb, lb;
+    uint32_t pix_count = src_len / 2;
+
+    for(uint32_t i = 0; i < pix_count; i ++) {
+        hb = *src_buf++;
+        lb = *src_buf++;
+
+        *dst_buf++ = hb & 0xF8;
+        *dst_buf++ = (hb & 0x07) << 5 | (lb & 0xE0) >> 3;
+        *dst_buf++ = (lb & 0x1F) << 3;
+    }
+
+    return true;
+}

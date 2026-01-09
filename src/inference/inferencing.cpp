e59dd99399c42fde5c880aaaa06bee8e500d9550
francovaro <francovaro@gmail.com>
2026-01-09 11:53:16 +0100

    removed debug msg

--- src/inference/inferencing.cpp
+++ src/inference/inferencing.cpp
@@ -170,8 +170,7 @@ static bool ei_stop_impulse(void)
  * @return
  */
 static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
-{
-    ei_printf(" ---- ei_camera_get_data offset %d length %d\n", offset, length);
+{    
     // we already have a RGB888 buffer, so recalculate offset into pixel index
     size_t pixel_ix = offset * 3;
     size_t pixels_left = length;
